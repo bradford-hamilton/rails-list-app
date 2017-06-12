d652b0ca7ad0fc0c4355dc77fe372730737e181d
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-06-11 18:56:44 -0600

    add List component

--- app/assets/javascripts/components/List.js.jsx
+++ app/assets/javascripts/components/List.js.jsx
@@ -1,9 +1,23 @@
 class List extends React.Component {
-   constructor(props) {
-       super(props);
-   }
+  constructor(props) {
+    super(props);
 
-   render() {
-       return <h1>React List Component</h1>;
-   }
+    this.directToList = this.directToList.bind(this);
+  }
+
+  directToList(list) {
+    window.location = `/lists/${list.id}`;
+  }
+
+  get Lists() {
+    return this.props.lists.map((list, i) => {
+      return (
+        <li key={i} onClick={() => {this.directToList(list)}}>{list.name}</li>
+      )
+    });
+  }
+
+  render() {
+    return <ol>{this.Lists}</ol>;
+  }
 }
